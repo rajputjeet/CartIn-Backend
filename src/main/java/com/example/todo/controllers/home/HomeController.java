f19bb227ee21d67aa1c5e97fa52c18e20f464d50
rajputjeet <rajputjeet034@gmail.com>
2026-03-11 18:02:11 +0530

    restore project files from commit 03ac24d

--- src/main/java/com/example/todo/controllers/home/HomeController.java
+++ src/main/java/com/example/todo/controllers/home/HomeController.java
@@ -1,18 +1,14 @@
 package com.example.todo.controllers.home;
 
 import com.example.todo.dto.home.HomeResponseDto;
-import com.example.todo.dto.home.categoryDto.subCategory.SubCategoryResponseDto;
 import com.example.todo.entities.response.ApiResponse;
 import com.example.todo.services.home.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/home")
@@ -35,20 +31,6 @@ public class HomeController {
     }
 
 
-    @GetMapping()
-    public ResponseEntity<ApiResponse<List<SubCategoryResponseDto>>> getSubCategoriesByParent(@PathVariable Long parentId){
 
-        List<SubCategoryResponseDto> subcategories = homeService.getSubCategoriesByParent(parentId);
-
-        ApiResponse<List<SubCategoryResponseDto>> response = new ApiResponse<>(
-                "success",
-                HttpStatus.OK.value(),
-                "Categories fetched successfully",
-                subcategories
-        );
-
-        return ResponseEntity.ok(response);
-
-    }
 
 }
